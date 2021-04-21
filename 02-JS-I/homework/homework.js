4d31e267b929117641bbdcefc476638b6fefe756
teranej <teranej@gmail.com>
2021-04-21 13:18:16 -0300

    homework actualizado

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -75,29 +75,50 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
+  return str1.length === str2.length
+
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  if (num < 90) {
+    return true
+  }
+  else {return false}
+
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  if (num>50){
+    
+  return true 
+  }else {return false}
 }
+
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  return(x % y)
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  if (num%2===0){
+    return true
+  }
+  else {return false}
+
 }
 
 function esImpar(num) {
