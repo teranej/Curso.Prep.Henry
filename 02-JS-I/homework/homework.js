3e6f8c51d61aaeb6e8e9b199eee821a7edace7ed
teranej <teranej@gmail.com>
2021-04-22 02:02:03 -0300

    homework actualizado hasta agregar ! al string

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -198,6 +198,8 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+
+  return str +'!'
 }
 
 function combinarNombres(nombre, apellido) {
