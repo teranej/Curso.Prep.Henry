c68662510f95f93c9a74e7bd9240ea18765132b1
teranej <teranej@gmail.com>
2021-05-19 21:01:04 -0300

    hw actualizada hasta operadores logicos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -55,8 +55,8 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
 
-  if (idioma ==="aleman"){
-    return "Guten Tag"
+  if (idioma === "aleman"){
+    return "Guten Tag!"
   }
   if (idioma === "mandarin"){
     return "Ni Hao!"
@@ -77,18 +77,41 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+
+  switch(color){
+    case "blue":
+      return "This is blue"
+    case "red":
+      return 'This is red'
+    case 'green':
+      return 'This is green'
+    case 'orange':
+      return 'This is orange'
+    default:
+      return 'Color not found'
+
+      
+    
+  
+  }
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return numero === 5 || numero === 10
+
 }
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+
+  return numero < 50 && numero > 20
+
+
 }
 
 function esEntero(numero) {
@@ -99,6 +122,9 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+
+  return numero % 1 === 0
+
 }
 
 function fizzBuzz(numero) {
@@ -106,7 +132,23 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-}
+
+  if(numero % 3 === 0 && numero % 5 ===0 ){
+    return "fizzbuzz"
+  }
+  else if (numero % 3 === 0){
+    return "fizz"
+  }
+  else if (numero % 5 === 0){
+    return "buzz" 
+
+  }
+  else{
+    return numero
+  }
+  
+  
+  }
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
@@ -115,6 +157,22 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1<0 || num2<0 || num3<0){
+    return "Hay negativos"
+  }
+  else if (num1===0 || num2===0 || num3===0){
+    return "Error"
+  }
+  else if (num1>num2 && num1>num3 && num1>0){
+    return "Número 1 es mayor y positivo"
+  }
+  else if (num3>num1 && num3>num2){
+    return num3+1
+  }
+  else{
+    return false
+  }
+  
 }
 
 function esPrimo(numero) {
